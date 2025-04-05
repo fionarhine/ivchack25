250b7cbddedc71c8bad0585f927fab39d17b4402
fionarhine <wangsawidjajafiona@gmail.com>
2025-04-05 10:41:14 -0700

    options menu buttons

---
+++ grow/Assets/back.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class back : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadSceneAsync("Main menu");
+    }
+
+    public void chatLoad(){
+        SceneManager.LoadSceneAsync("chatting area");
+    }
+
+    public void resourceLoad(){
+        SceneManager.LoadSceneAsync("Resource comments");
+    }
+
+    public void studyLoad(){
+        SceneManager.LoadSceneAsync("study area");
+    }
+}
